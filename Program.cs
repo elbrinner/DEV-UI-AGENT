fa1363fb102aa922754478b162ad9bc23f013561
Elbrinner da Silva Fernandes <elbrinner@elbrinner.com>
2025-12-14 07:36:44 +0100

    feat: Añadir configuración del cliente Azure OpenAI y registro del agente de chat

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
 // Namespaces del Agent Framework y UI Dev
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Core;
 using Microsoft.Agents;
 using Microsoft.Agents.AI;
 using Microsoft.Agents.AI.DevUI;
 using Microsoft.Agents.AI.Hosting;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
+using System.ClientModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuraci�n: appsettings + variables de entorno
+// Configuración: appsettings + variables de entorno
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.Configuration.AddEnvironmentVariables();
+
+// Configurar Azure OpenAI Client
+var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"];
+var apikey = builder.Configuration["AZURE_OPENAI_API_KEY"];
+var deployment = builder.Configuration["AZURE_OPENAI_DEPLOYMENT_NAME"] ?? "gpt-4.1-mini";
+
+var chatClient = new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(apikey))
+    .GetChatClient(deployment)
+    .AsIChatClient();
+
+builder.Services.AddChatClient(chatClient);
+
+builder.AddAIAgent("ChatAgent", (sp, key) =>
+{
+    // get logger
+    var logger = sp.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Configuring AI Agent with key '{Key}' for model '{Model}'", key, "gpt-5-mini");
+
+
+
+    // create agent
+    var chatClient = sp.GetRequiredService<IChatClient>();
+    var aiAgent = chatClient.CreateAIAgent(
+        name: key,
+        instructions: "You are an useful agent that helps users with short and funny answers.",
+        description: "An AI agent that helps users with short and funny answers.",
+        tools: []
+        )
+    .AsBuilder()
+    .UseOpenTelemetry(configure: c =>
+        c.EnableSensitiveData = builder.Environment.IsDevelopment())
+    .Build();
+    return aiAgent;
+});
 
 // Registrar servicios requeridos por Dev UI y endpoints de OpenAI
 builder.Services.AddOpenAIResponses();
@@ -17,7 +56,7 @@ builder.Services.AddOpenAIConversations();
 
 var app = builder.Build();
 
-// Pipeline HTTP m�nimo
+// Pipeline HTTP mínimo
 app.UseHttpsRedirection();
 
 // Mapear endpoints necesarios para respuestas y conversaciones (usados por Dev UI)
